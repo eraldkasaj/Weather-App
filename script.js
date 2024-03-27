e7d395bdd078d9a24ebf9c50db0b32c13414fd59
Aldi Kasaj <eraldkasaj14@gmail.com>
2024-03-27 01:02:35 +0100

    new update 2.0

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 
 const apiKey = "adc2740c7ed768a9e88a7411361bd4c3"
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=london"
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q="
+const searchBtn = document.querySelector('.search button')
+const inputSearch = document.querySelector('.search input')
+const weatherImg = document.querySelector('.weather-icon')
 
+ 
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    const data = await response.json()
-    console.log(data)
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`)
 
-    document.querySelector('.city').innerHTML =data.name
-    document.querySelector('.temp').innerHTML =data.main.temp
-    document.querySelector('.humidity').innerHTML =data.main.humidity
-    document.querySelector('.wind').innerHTML =data.wind.speed
+    if(response.status == 404) {
+        document.querySelector('.error').style.display='block'
+        document.querySelector('.weather').style.display='none'
+    }/*else if(response.status == 400){
+        document.querySelector('.error').style.display='block'
+        document.querySelector('.error').innerHTML='put a city name'
+    }*/
+    else {
+        const data = await response.json()
+        console.log(data)
+    
+        document.querySelector('.city').innerHTML =data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + `°C`
+        document.querySelector('.humidity').innerHTML =data.main.humidity + `%`
+        document.querySelector('.wind').innerHTML =data.wind.speed + `km/h`
+    
+        if(data.weather[0].main == 'Clear') {
+            weatherImg.src ="/images/clear.png"
+        }else if(data.weather[0].main == 'Clouds') {
+            weatherImg.src ="/images/clouds.png"
+        }else if(data.weather[0].main == 'Rain') {
+            weatherImg.src ="/images/rain.png"
+        }else if(data.weather[0].main == 'Drizzle') {
+            weatherImg.src ="/images/drizzle.png"
+        }
+
+        document.querySelector('.weather').style.display='block'
+        document.querySelector('.error').style.display='none'
+    }
+
+   
 }
 
-checkWeather()
 
-checkWeather()
-checkWeather()
-checkWeather(b)
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(inputSearch.value)
+})
 
